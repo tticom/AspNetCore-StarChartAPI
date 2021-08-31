c9ea98773bb6d40aee8b3cb691107e51662cb866
tticom <niall.finlay@gmail.com>
2021-08-31 12:50:07 +0100

    fixes

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
