859e68fb82b38502bce53233a0611818f439b3c4
tticom <niall.finlay@gmail.com>
2021-08-31 12:59:45 +0100

    controller

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+    }
+}
